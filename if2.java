18a9913e16df2729cda2340d0015b144bfaec497
Niyobyose <150947918+Niyobyose@users.noreply.github.com>
2023-11-20 01:15:46 -0800

    Add files via upload

---
+++ if2.java
@@ -0,0 +1,13 @@
+class IfDemo {
+    public static void main(String args[])
+    {
+        String str = "GeeksforGeeks";
+        int i = 4;
+        if (i == 4) {
+            i++;
+            System.out.println(str);
+        }
+ 
+        System.out.println("i = " + i);
+    }
+}
